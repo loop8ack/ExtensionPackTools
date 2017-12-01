d3971e3aa309ffcbbadaa09c8c705edc361286f9
Mads Kristensen <post@madskristensen.net>
2017-12-01 11:04:56 -0800

    Add project files.

---
+++ src/Commands/ExportCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace ExtensionPackTools
+{
+    internal sealed class ExportCommand
+    {
+        private readonly Package _package;
+
+        private ExportCommand(Package package)
+        {
+            _package = package ?? throw new ArgumentNullException("package");
+
+            if (ServiceProvider.GetService(typeof(IMenuCommandService)) is OleMenuCommandService commandService)
+            {
+                var menuCommandID = new CommandID(PackageGuids.guidExportPackageCmdSet, PackageIds.ExportCmd);
+                var menuItem = new MenuCommand(Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        public static ExportCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        private IServiceProvider ServiceProvider
+        {
+            get { return _package; }
+        }
+
+        public static void Initialize(Package package)
+        {
+            Instance = new ExportCommand(package);
+        }
+
+        private void Execute(object sender, EventArgs e)
+        {
+            string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", GetType().FullName);
+            string title = "Export";
+
+            VsShellUtilities.ShowMessageBox(
+                this.ServiceProvider,
+                message,
+                title,
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+        }
+    }
+}
---
+++ src/VsPackage.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+
+namespace ExtensionPackTools
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", Vsix.Version, IconResourceID = 400)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.guidVsPackageString)]
+    public sealed class VsPackage : Package
+    {
+        protected override void Initialize()
+        {
+            ExportCommand.Initialize(this);
+        }
+    }
+}
