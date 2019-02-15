c89fb1e2a1cdb31fe9c0659b91db1e17e3ba35b9
Mads Kristensen <post@madskristensen.net>
2019-02-15 14:24:58 -0800

    Added shared project and 2019 specific projet

--- src/VS2017/source.extension.cs
+++ src/VS2017/source.extension.cs
@@ -8,7 +8,7 @@ namespace ExtensionManager
     internal sealed partial class Vsix
     {
         public const string Id = "e83d71b8-8bfc-4e06-b145-b0388910c016";
-        public const string Name = "Extension Manager";
+        public const string Name = "Extension Manager 2017";
         public const string Description = @"Import/export extensions as well as associate extensions with individual solutions";
         public const string Language = "en-US";
         public const string Version = "0.5.999";
