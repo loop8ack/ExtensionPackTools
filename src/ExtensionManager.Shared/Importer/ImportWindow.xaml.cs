4f06c749a48c621f2f318f2a29310e90be96bb9f
Brian Hart <brian.hart@xylogix.net>
2020-12-27 14:04:45 -0500

    Fix #31

--- src/ExtensionManager.Shared/Importer/ImportWindow.xaml.cs
+++ src/ExtensionManager.Shared/Importer/ImportWindow.xaml.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interop;
+using EnvDTE;
 using Microsoft.VisualStudio.Shell;
+using Window = System.Windows.Window;
 
 namespace ExtensionManager.Importer
 {
@@ -13,7 +15,8 @@ namespace ExtensionManager.Importer
         private readonly IEnumerable<Extension> _extensions;
         private readonly Purpose _purpose;
 
-        public ImportWindow(IEnumerable<Extension> extensions, Purpose purpose, string text = null)
+        public ImportWindow(IEnumerable<Extension> extensions, Purpose purpose,
+            string text = null)
         {
             _extensions = extensions;
             _purpose = purpose;
@@ -21,13 +24,13 @@ namespace ExtensionManager.Importer
             InitializeComponent();
             Title = Vsix.Name;
 
-            if (!string.IsNullOrEmpty(text))
-            {
-                lblMessage.Content = text;
-            }
+            if (!string.IsNullOrEmpty(text)) lblMessage.Content = text;
 
-            btnOk.Content = purpose == Purpose.Install ? "Install..." : "Select";
-            chkInstallSystemWide.Visibility = purpose == Purpose.Install ? Visibility.Visible : Visibility.Hidden;
+            btnOk.Content =
+                purpose == Purpose.Install ? "Install..." : "Select";
+            chkInstallSystemWide.Visibility = purpose == Purpose.Install
+                ? Visibility.Visible
+                : Visibility.Hidden;
         }
 
         public List<Extension> SelectedExtension { get; private set; }
@@ -37,17 +40,15 @@ namespace ExtensionManager.Importer
         private void ImportWindow_Loaded(object sender, RoutedEventArgs e)
         {
             var hasCategory = false;
-            IEnumerable<Extension> sortedList = _extensions;
+            var sortedList = _extensions;
 
             if (_purpose == Purpose.Install)
-            {
-                sortedList = _extensions.OrderByDescending(x => x.Selected).ThenBy(x => x.Name);
-            }
+                sortedList = _extensions.OrderByDescending(x => x.Selected)
+                    .ThenBy(x => x.Name);
 
             if (_extensions.FirstOrDefault()?.Selected == true)
             {
-                var label = new Label
-                {
+                var label = new Label {
                     Content = "Extensions",
                     Margin = new Thickness(0, 0, 0, 5),
                     FontWeight = FontWeights.Bold
@@ -56,14 +57,13 @@ namespace ExtensionManager.Importer
                 list.Children.Add(label);
             }
 
-            foreach (Extension ext in sortedList)
+            foreach (var ext in sortedList)
             {
-                var cb = new CheckBox
-                {
+                var cb = new CheckBox {
                     Content = ext.Name,
                     IsChecked = ext.Selected,
                     CommandParameter = ext.ID,
-                    Margin = new Thickness(10, 0, 0, 0),
+                    Margin = new Thickness(10, 0, 0, 0)
                 };
 
                 if (_purpose == Purpose.Install && !ext.Selected)
@@ -72,8 +72,7 @@ namespace ExtensionManager.Importer
                     {
                         hasCategory = true;
 
-                        var label = new Label
-                        {
+                        var label = new Label {
                             Content = "Already installed",
                             Margin = new Thickness(0, 10, 0, 5),
                             FontWeight = FontWeights.Bold
@@ -89,28 +88,36 @@ namespace ExtensionManager.Importer
             }
         }
 
+        private void chkSelectDeselectAll_CheckedChanged(object sender,
+            RoutedEventArgs e)
+        {
+            foreach (var cb in list.Children.OfType<CheckBox>())
+                cb.IsChecked = chkSelectDeselectAll.IsChecked;
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             SelectedExtension = new List<Extension>();
 
-            foreach (CheckBox cb in list.Children.OfType<CheckBox>())
-            {
-                if (cb.IsChecked == true && cb.IsEnabled == true)
-                {
-                    SelectedExtension.Add(_extensions.First(ext => ext.ID == (string)cb.CommandParameter));
-                }
-            }
+            foreach (var cb in list.Children.OfType<CheckBox>())
+                if (cb.IsChecked == true && cb.IsEnabled)
+                    SelectedExtension.Add(
+                        _extensions.First(
+                            ext => ext.ID == (string)cb.CommandParameter
+                        )
+                    );
 
             InstallSystemWide = chkInstallSystemWide.IsChecked ?? false;
             DialogResult = true;
             Close();
         }
 
-        public static ImportWindow Open(IEnumerable<Extension> extensions, Purpose purpose, string msg = null)
+        public static ImportWindow Open(IEnumerable<Extension> extensions,
+            Purpose purpose, string msg = null)
         {
             ThreadHelper.ThrowIfNotOnUIThread();
 
-            var dte = Package.GetGlobalService(typeof(EnvDTE.DTE)) as EnvDTE.DTE;
+            var dte = Package.GetGlobalService(typeof(DTE)) as DTE;
 
             var dialog = new ImportWindow(extensions, purpose, msg);
             var hwnd = new IntPtr(dte.MainWindow.HWnd);
