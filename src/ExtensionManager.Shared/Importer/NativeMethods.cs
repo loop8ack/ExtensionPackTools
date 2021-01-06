a0d26814310c122c7e07529cbb19c7489b89da15
Brian Hart <brian.hart@xylogix.net>
2021-01-06 09:06:10 -0500

    Create methods in NativeMethods class
    Thanks to pinvoke.net and stack overflow

--- src/ExtensionManager.Shared/Importer/NativeMethods.cs
+++ src/ExtensionManager.Shared/Importer/NativeMethods.cs
@@ -1,10 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
 
 namespace ExtensionManager.Importer
 {
+    /// <summary>
+    /// Provides definitions for Win32 API methods and constants.
+    /// </summary>
     internal static class NativeMethods
     {
+        /// <summary>
+        /// Associates a new large or small icon with a window. The system displays the large icon in the ALT+TAB dialog box, and the small icon in the window caption.
+        /// </summary>
+        public const int WM_SETICON = 0x0080;
+
+        // from winuser.h
+        public const int GWL_STYLE = -16,
+                      WS_MAXIMIZEBOX = 0x10000,
+                      WS_MINIMIZEBOX = 0x20000;
+
+        [DllImport("user32.dll")]
+        internal extern static int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        internal extern static int SetWindowLong(IntPtr hwnd, int index, int value);
+
+        // thanks stack overflow <https://stackoverflow.com/questions/339620/how-do-i-remove-minimize-and-maximize-from-a-resizable-window-in-wpf>
+        /// <summary>
+        /// Removes the Minimize button from the window's title bar. This must be done since there is no Taskbar icon being displayed to which  the user might minimize the window.
+        /// </summary>
+        /// <remarks>We also do this in order to style the window in a manner akin to the Visual Studio 2019 Create New Project window (for consistent look/feel).</remarks>
+        internal static void HideMinimizeButton(this Window window)
+        {
+            if (window == null) return;
+
+            var hwnd = new WindowInteropHelper(window).Handle;
+
+            if (!IsWindow(hwnd)) return;
+
+            var currentStyle = GetWindowLong(hwnd, GWL_STYLE);
+
+            SetWindowLong(hwnd, GWL_STYLE, (currentStyle & ~WS_MINIMIZEBOX));
+        }
+
+        /// <summary>
+        /// Determines whether the specified window handle identifies an existing window.
+        /// </summary>
+        /// <param name="hWnd">A handle to the window to be tested.</param>
+        /// <returns>If the window handle identifies an existing window, the return value is nonzero. If the window handle does not identify an existing window, the return value is zero.</returns>
+        /// <remarks>A thread should not use IsWindow for a window that it did not create because the window could be destroyed after this function was called. Further, because window handles are recycled the handle could even point to a different window.</remarks>
+        [DllImport("user32.dll")]
+        internal static extern bool IsWindow(IntPtr hWnd);
+
+        /// <summary>
+        /// Sends the specified message to a window or windows. The SendMessage function calls the window procedure for the specified window and does not return until the window procedure has processed the message.<para>To send a message and return immediately, use the SendMessageCallback or SendNotifyMessage function. To post a message to a thread's message queue and return immediately, use the PostMessage or PostThreadMessage function.</para>
+        /// </summary>
+        /// <param name="hWnd">A handle to the window whose window procedure will receive the message. If this parameter is HWND_BROADCAST ((HWND)0xffff), the message is sent to all top-level windows in the system, including disabled or invisible unowned windows, overlapped windows, and pop-up windows; but the message is not sent to child windows.<para/>Message sending is subject to UIPI. The thread of a process can send messages only to message queues of threads in processes of lesser or equal integrity level.</param>
+        /// <param name="Msg">The message to be sent.<para/>For lists of the system-provided messages, see System-Defined Messages.</param>
+        /// <param name="wParam">Additional message-specific information.</param>
+        /// <param name="lParam">Additional message-specific information.</param>
+        /// <returns></returns>
+        /// <returns>The return value specifies the result of the message processing; it depends on the message sent.</returns>
+        /// <remarks>When a message is blocked by UIPI the last error, retrieved with GetLastError, is set to 5 (access denied).<para/>Applications that need to communicate using HWND_BROADCAST should use the RegisterWindowMessage function to obtain a unique message for inter-application communication.<para/>The system only does marshalling for system messages (those in the range 0 to (WM_USER-1)). To send other messages (those >= WM_USER) to another process, you must do custom marshalling.<para/>If the specified window was created by the calling thread, the window procedure is called immediately as a subroutine. If the specified window was created by a different thread, the system switches to that thread and calls the appropriate window procedure. Messages sent between threads are processed only when the receiving thread executes message retrieval code. The sending thread is blocked until the receiving thread processes the message. However, the sending thread will process incoming nonqueued messages while waiting for its message to be processed. To prevent this, use SendMessageTimeout with SMTO_BLOCK set. For more information on nonqueued messages, see Nonqueued Messages.<para/>An accessibility application can use SendMessage to send WM_APPCOMMAND messages to the shell to launch applications. This functionality is not guaranteed to work for other types of applications.</remarks>
+        [DllImport("coredll.dll")]
+        internal static extern IntPtr SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
     }
 }
