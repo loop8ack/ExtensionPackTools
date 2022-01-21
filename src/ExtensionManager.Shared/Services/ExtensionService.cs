030f420ae6f9ce7907e6c64bdd4e2aaf7830d852
Brian Hart <brian.hart@xylogix.net>
2022-01-20 22:05:29 -0600

    Update ExtensionService.cs
    * Pretty-printed the code in accordance with .editorconfig and ReSharper code style settings with the objective of keeping a 80-char line width if possible.  This is for better readability on GitHub.

--- src/ExtensionManager.Shared/Services/ExtensionService.cs
+++ src/ExtensionManager.Shared/Services/ExtensionService.cs
@@ -10,7 +10,8 @@ namespace ExtensionManager
         private readonly IVsExtensionManager _manager;
         private readonly IVsExtensionRepository _repository;
 
-        public ExtensionService(IVsExtensionManager manager, IVsExtensionRepository repository)
+        public ExtensionService(IVsExtensionManager manager,
+            IVsExtensionRepository repository)
         {
             _manager = manager;
             _repository = repository;
@@ -21,15 +22,21 @@ namespace ExtensionManager
             ThreadHelper.ThrowIfNotOnUIThread();
 
             var installed = _manager.GetInstalledExtensions()
-                                       .Where(i => !i.Header.SystemComponent && !i.IsPackComponent)
-                                       .Select(i => i.Header.Identifier)
-                                       .ToList();
+                                    .Where(
+                                        i => !i.Header.SystemComponent &&
+                                             !i.IsPackComponent
+                                    )
+                                    .Select(i => i.Header.Identifier)
+                                    .ToList();
 
             // Filter the installed extensions to only be the ones that exist on the Marketplace
-            IEnumerable<GalleryEntry> marketplaceEntries = _repository.GetVSGalleryExtensions<GalleryEntry>(installed, 1033, false);
+            var marketplaceEntries =
+                _repository.GetVSGalleryExtensions<GalleryEntry>(
+                    installed, 1033, false
+                );
             return marketplaceEntries
-                .Select(e => Extension.FromGalleryExtension(e))
-                .OrderBy(e => e.Name);
+                   .Select(e => Extension.FromGalleryExtension(e))
+                   .OrderBy(e => e.Name);
         }
     }
 }
