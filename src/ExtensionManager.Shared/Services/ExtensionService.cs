441667c32879ed7a02dbc565241c952c88c464de
Brian Hart <brian.hart@xylogix.net>
2022-01-20 23:18:38 -0600

    Update ExtensionService.cs
    Extracted an 'IExtensionService' interface.

--- src/ExtensionManager.Shared/Services/ExtensionService.cs
+++ src/ExtensionManager.Shared/Services/ExtensionService.cs
@@ -10,10 +10,32 @@ using IExtension = ExtensionManager.Core.Models.Interfaces.IExtension;
 namespace ExtensionManager
 {
     /// <summary>
+    /// Defines the publicly-exposed methods and properties of an object that allows
+    /// access to the list of installed extensions.
+    /// </summary>
+    public interface IExtensionService
+    {
+        /// <summary>
+        /// Obtains a list of all the extensions installed in this instance of Visual
+        /// Studio that were obtained from the Visual Studio Marketplace.
+        /// </summary>
+        /// <returns>
+        /// Collection of instances of objects that implement the
+        /// <see cref="T:ExtensionManager.IExtension" />
+        /// , one for each of the extensions that are installed, which are initialized with
+        /// the extension metadata.
+        /// <para />
+        /// If no extensions obtained from the Visual Studio Marketplace are installed, or
+        /// if an error occurs, then the empty collection is returned.
+        /// </returns>
+        IEnumerable<IExtension> GetInstalledExtensions();
+    }
+
+    /// <summary>
     /// Service that provides access to extension metadata from the Visual Studio
     /// Marketplace.
     /// </summary>
-    public class ExtensionService
+    public class ExtensionService : IExtensionService
     {
         /// <summary>
         /// Reference to an instance of an object that implements the
@@ -55,7 +77,8 @@ namespace ExtensionManager
         /// Studio that were obtained from the Visual Studio Marketplace.
         /// </summary>
         /// <returns>
-        /// Collection of instances of objects that implement the <see cref="T:ExtensionManager.IExtension" />
+        /// Collection of instances of objects that implement the
+        /// <see cref="T:ExtensionManager.IExtension" />
         /// , one for each of the extensions that are installed, which are initialized with
         /// the extension metadata.
         /// <para />
