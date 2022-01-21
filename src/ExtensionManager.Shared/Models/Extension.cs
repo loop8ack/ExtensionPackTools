1343e73c9efb94d046fbed06eacca1c31ecc8edc
Brian Hart <brian.hart@xylogix.net>
2022-01-20 23:01:52 -0600

    Move the IExtension interface to its own file

--- src/ExtensionManager.Shared/Models/Extension.cs
+++ src/ExtensionManager.Shared/Models/Extension.cs
@@ -5,40 +5,6 @@ using Newtonsoft.Json;
 namespace ExtensionManager
 {
     /// <summary>
-    /// Defines the publicly-exposed methods and properties of a POCO that encapsulates
-    /// metadata about an extension.
-    /// </summary>
-    public interface IExtension
-    {
-        /// <summary>
-        /// Gets or sets a string containing the URL from which the extension can be
-        /// downloaded.
-        /// </summary>
-        string DownloadUrl { get; set; }
-
-        /// <summary>
-        /// Gets or sets the identifier for the extension.
-        /// </summary>
-        string ID { get; set; }
-
-        /// <summary>
-        /// Gets or sets the URL of the page on the Visual Studio Marketplace website for
-        /// the extension.
-        /// </summary>
-        string MoreInfoUrl { get; set; }
-
-        /// <summary>
-        /// Gets or sets a string containing the name of the extension.
-        /// </summary>
-        string Name { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value indicating whether this extension is selected.
-        /// </summary>
-        bool Selected { get; set; }
-    }
-
-    /// <summary>
     /// Provides information about an extension for export to a JSON-formatted file
     /// with a <c>.vsext</c> extension.
     /// </summary>
---
+++ src/ExtensionManager.Shared/Models/IExtension.cs
@@ -0,0 +1,36 @@
+﻿namespace ExtensionManager
+{
+    /// <summary>
+    /// Defines the publicly-exposed methods and properties of a POCO that encapsulates
+    /// metadata about an extension.
+    /// </summary>
+    public interface IExtension
+    {
+        /// <summary>
+        /// Gets or sets a string containing the URL from which the extension can be
+        /// downloaded.
+        /// </summary>
+        string DownloadUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identifier for the extension.
+        /// </summary>
+        string ID { get; set; }
+
+        /// <summary>
+        /// Gets or sets the URL of the page on the Visual Studio Marketplace website for
+        /// the extension.
+        /// </summary>
+        string MoreInfoUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets a string containing the name of the extension.
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this extension is selected.
+        /// </summary>
+        bool Selected { get; set; }
+    }
+}
