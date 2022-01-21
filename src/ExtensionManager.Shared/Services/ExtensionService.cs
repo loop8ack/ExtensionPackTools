d442dca89fc96da3d6b900c9cf8829db08188946
Brian Hart <brian.hart@xylogix.net>
2022-01-20 23:19:09 -0600

    Move the IExtensionService interface to its own file

--- src/ExtensionManager.Shared/Services/ExtensionService.cs
+++ src/ExtensionManager.Shared/Services/ExtensionService.cs
@@ -10,28 +10,6 @@ using IExtension = ExtensionManager.Core.Models.Interfaces.IExtension;
 namespace ExtensionManager
 {
     /// <summary>
-    /// Defines the publicly-exposed methods and properties of an object that allows
-    /// access to the list of installed extensions.
-    /// </summary>
-    public interface IExtensionService
-    {
-        /// <summary>
-        /// Obtains a list of all the extensions installed in this instance of Visual
-        /// Studio that were obtained from the Visual Studio Marketplace.
-        /// </summary>
-        /// <returns>
-        /// Collection of instances of objects that implement the
-        /// <see cref="T:ExtensionManager.IExtension" />
-        /// , one for each of the extensions that are installed, which are initialized with
-        /// the extension metadata.
-        /// <para />
-        /// If no extensions obtained from the Visual Studio Marketplace are installed, or
-        /// if an error occurs, then the empty collection is returned.
-        /// </returns>
-        IEnumerable<IExtension> GetInstalledExtensions();
-    }
-
-    /// <summary>
     /// Service that provides access to extension metadata from the Visual Studio
     /// Marketplace.
     /// </summary>
