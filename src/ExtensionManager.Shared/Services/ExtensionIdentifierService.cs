da51ef64a1bfee5b169ccee63b5635d5c99d9c64
Brian Hart <brian.hart@xylogix.net>
2022-01-22 20:05:26 -0600

    Remove Get class; bring ExtensionIdentifierService class back
    * This class will need to inject the IVsManager, so it cannot be a static class nor a singleton

---
+++ src/ExtensionManager.Shared/Services/ExtensionIdentifierService.cs
@@ -0,0 +1,14 @@
+﻿namespace ExtensionManager
+{
+    /// <summary>
+    /// Exposes methods to obtain information from external data sources (UI/UX, Visual
+    /// Studio, etc.)
+    /// </summary>
+    /// <remarks>
+    /// This class is in charge of getting the identifiers of those extensions
+    /// that are (a) obtained from the Visual Studio Marketplace, (b) are not system
+    /// components, and (c) is not a component of a package (<c>IsPackComponent</c> is
+    /// <see langword="false" />).
+    /// </remarks>
+    public class ExtensionIdentifierService { }
+}
