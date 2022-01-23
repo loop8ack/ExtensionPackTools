5b2bc8a7b139ec8519fcb4e59936a62de36014c7
Brian Hart <brian.hart@xylogix.net>
2022-01-22 20:00:38 -0600

    Create ExtensionIdentifierService.cs
    This class is in charge of getting the identifiers of those extensions that are (a) obtained from the Visual Studio Marketplace, (b) are not system components, and (c) is not a component of a package (IsPackComponent is false).

---
+++ src/ExtensionManager.Shared/Services/ExtensionIdentifierService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionManager.Shared.Services
+{
+    /// <summary>
+    /// Exposes methods to obtain extension identifiers.
+    /// </summary>
+    /// <remarks>This class is in charge of getting the identifiers of those extensions that are (a) obtained from the Visual Studio Marketplace, (b) are not system components, and (c) is not a component of a package (<c>IsPackComponent</c> is <see langword="false" />).</remarks>
+    public class ExtensionIdentifierService {
+        // TODO: Add class content here
+    }
+}
