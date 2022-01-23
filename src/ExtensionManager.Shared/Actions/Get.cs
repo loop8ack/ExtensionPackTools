1235478e8434ef2205dd084419ca035171f61e96
Brian Hart <brian.hart@xylogix.net>
2022-01-22 20:02:38 -0600

    Renamed ExtensionIdentifierService to Get and moved it to the Actions folder.
    * This class can expose several fluent, static methods to obtain various kinds of information.

---
+++ src/ExtensionManager.Shared/Actions/Get.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionManager
+{
+    /// <summary>
+    /// Exposes methods to obtain information from external data sources (UI/UX, Visual Studio, etc.)
+    /// </summary>
+    public static class Get {
+        
+    }
+}
