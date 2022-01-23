82b60061e37507c044c208be309e2356395de704
Brian Hart <brian.hart@xylogix.net>
2022-01-22 20:12:03 -0600

    Update ExtensionIdentifierService.cs
    * Implemented the class' functionality, paying attention to possible unanticipated outcomes of method calls to the Visual Studio Extension Manager API and handling exceptions by returning the empty collection.

--- src/ExtensionManager.Shared/Services/ExtensionIdentifierService.cs
+++ src/ExtensionManager.Shared/Services/ExtensionIdentifierService.cs
@@ -1,4 +1,10 @@
-﻿namespace ExtensionManager
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.VisualStudio.ExtensionManager;
+
+namespace ExtensionManager
 {
     /// <summary>
     /// Exposes methods to obtain information from external data sources (UI/UX, Visual
@@ -10,5 +16,69 @@
     /// components, and (c) is not a component of a package (<c>IsPackComponent</c> is
     /// <see langword="false" />).
     /// </remarks>
-    public class ExtensionIdentifierService { }
+    public class ExtensionIdentifierService
+    {
+        /// <summary>
+        /// Reference to an instance of an object that implements the
+        /// <see cref="T:Microsoft.VisualStudio.ExtensionManager.IVsExtensionManager" />
+        /// interface.
+        /// </summary>
+        private readonly IVsExtensionManager _manager;
+
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see cref="T:ExtensionManager.ExtensionIdentifierService" /> and returns a
+        /// reference to it.
+        /// </summary>
+        /// <param name="manager">
+        /// (Required.) Reference to an instance of an object that implements the
+        /// <see cref="T:Microsoft.VisualStudio.ExtensionManager.IVsExtensionManager" />
+        /// interface.
+        /// </param>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// Thrown if the required
+        /// parameter, <paramref name="manager" />, is passed a <see langword="null" />
+        /// value.
+        /// </exception>
+        public ExtensionIdentifierService(IVsExtensionManager manager)
+        {
+            _manager = manager ??
+                       throw new ArgumentNullException(nameof(manager));
+        }
+
+        public IEnumerable<string> GetInstalledExtensionIdentifiers()
+        {
+            var result = Enumerable.Empty<string>();
+
+            try
+            {
+                if (_manager == null) return result;
+
+                // Check filtered result for having zero elements come back
+                // prior to projecting it.
+                if (!_manager.GetInstalledExtensions()
+                             .Any(
+                                 i => !i.Header.SystemComponent &&
+                                      !i.IsPackComponent
+                             ))
+                    return result;
+
+                result = _manager.GetInstalledExtensions()
+                        .Where(
+                            i => !i.Header.SystemComponent && !i.IsPackComponent
+                        )
+                        .Select(i => i.Header.Identifier)
+                        .ToList();
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the debugger
+                Debug.WriteLine(ex);
+
+                result = Enumerable.Empty<string>();
+            }
+
+            return result;
+        }
+    }
 }
