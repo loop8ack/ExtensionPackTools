44456d0f0b50418aaf1747916a973dad9fd82a3c
Brian Hart <brian.hart@xylogix.net>
2022-01-22 20:32:35 -0600

    Update ExtensionIdentifierService.cs
    Remove erroneously-added namespace using directive.

--- src/ExtensionManager.Shared/Services/ExtensionIdentifierService.cs
+++ src/ExtensionManager.Shared/Services/ExtensionIdentifierService.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using ExtensionManager.Shared.Helpers;
 using Microsoft.VisualStudio.ExtensionManager;
 
 namespace ExtensionManager
