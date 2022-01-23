9e1f536cb4fa5a35252757f8e456ea1ff566b976
Brian Hart <brian.hart@xylogix.net>
2022-01-22 20:28:12 -0600

    Create InstalledExtensionHelper.cs
    * This is a static class that exposes extension methods for objects that implement the Microsoft.VisualStudio.ExtensionManager.IInstalledExtension interface, so that calling code can be more fluent.

---
+++ src/ExtensionManager.Shared/Helpers/InstalledExtensionHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.ExtensionManager;
+
+namespace ExtensionManager.Shared.Helpers
+{
+    /// <summary>
+    /// Exposes static extension methods for objects that implement the
+    /// <see cref="T:Microsoft.VisualStudio.ExtensionManager.IInstalledExtension" />
+    /// interface.
+    /// </summary>
+    public static class InstalledExtensionHelper
+    {
+        /// <summary>
+        /// Safely obtains the identifier of the specified
+        /// <paramref name="installedExtension" />.
+        /// </summary>
+        /// <param name="installedExtension">
+        /// (Required.) Reference to an instance of an object that implements the
+        /// <see cref="T:Microsoft.VisualStudio.ExtensionManager.IInstalledExtension" />
+        /// interface.
+        /// </param>
+        /// <returns>
+        /// String containing the identifier of the specified
+        /// <paramref name="installedExtension" />.
+        /// <para />
+        /// If the method is passed a <see langword="null" /> value, or if the header
+        /// information is missing, then this method returns the empty string.
+        /// </returns>
+        public static string GetIdentiifer(
+            this IInstalledExtension installedExtension)
+        {
+            return installedExtension?.Header?.Identifier;
+        }
+    }
+}
