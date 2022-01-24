e4e362d1c75e03ee918d4a4366a966b1ae694b86
Brian Hart <brian.hart@xylogix.net>
2022-01-24 10:28:55 -0600

    Create CommandBase.cs
    * This is to be a shared, abstract base class for all the commands, into which the behaviors common to all the commands is to be moved.
    
    This is to follow the Template Method pattern.

---
+++ src/ExtensionManager.Shared/Commands/CommandBase.cs
@@ -0,0 +1,7 @@
+﻿namespace ExtensionManager
+{
+    /// <summary>
+    /// Defines the common events, methods, properties, and behaviors for all Commands.
+    /// </summary>
+    public abstract class CommandBase { }
+}
