adcce4eef4c0d6746218346c55f1d321a2c7ebd2
Brian Hart <brian.hart@xylogix.net>
2022-01-25 14:34:24 -0600

    Update Purpose.cs
    * Added XML documentation to the enumeration itself, as well as its members.

--- src/ExtensionManager.Shared/Importer/Purpose.cs
+++ src/ExtensionManager.Shared/Importer/Purpose.cs
@@ -1,9 +1,28 @@
 ﻿namespace ExtensionManager
 {
+    /// <summary>
+    /// Values that designate the particular operation that the user desires to perform
+    /// with the Importer.
+    /// </summary>
     public enum Purpose
     {
+        /// <summary>
+        /// Prompt the user to install extensions to Visual Studio because a particular
+        /// Solution (<c>.sln</c>) needs them.
+        /// </summary>
         InstallForSolution,
+
+        /// <summary>
+        /// Importing extensions into Visual Studio from a <c>.vsext</c> JSON-formatted
+        /// file that describes which extensions should be installed.
+        /// </summary>
         Import,
+
+        /// <summary>
+        /// Exporting a list of the currently-installed Visual Studio extensions (only
+        /// those that have been obtained from the Visual Studio Marketplace) and output
+        /// that list to a JSON-formatted file with a <c>.vsext</c> extension.
+        /// </summary>
         Export
     }
 }
