210317a04071af2025330c74b5c1e991de7fb89b
Brian Hart <brian.hart@xylogix.net>
2022-01-26 12:55:56 -0600

    Update Get.cs
    * Added XML documentation comments to the Get.ExportFileName method.

--- src/ExtensionManager.Shared/Actions/Get.cs
+++ src/ExtensionManager.Shared/Actions/Get.cs
@@ -4,8 +4,31 @@ using System.Windows.Forms;
 
 namespace ExtensionManager.Shared.Actions
 {
+    /// <summary>
+    /// Exposes static methods to obtain values.
+    /// </summary>
     public static class Get
     {
+        /// <summary>
+        /// Prompts the interactive user, with a Save As dialog box, to determine to which
+        /// pathname the user would like the export saved.
+        /// </summary>
+        /// <param name="initialDirectory">
+        /// (Optional.) The fully-qualified pathname of the
+        /// folder where the Save As dialog box should be focused when it initially opens.
+        /// <para />
+        /// If this value is blank, then the dialog box opens focused on the <c>This PC</c>
+        /// area of Windows instead.
+        /// </param>
+        /// <returns>
+        /// String containing the fully-qualified pathname that the user chose in
+        /// the dialog box; or, blank if an error occurred or the user clicked the
+        /// <strong>Cancel</strong> button.
+        /// </returns>
+        /// <remarks>
+        /// If the return value of this method is blank, then callers should not
+        /// proceed.
+        /// </remarks>
         public static string ExportFilePath(string initialDirectory = "")
         {
             var result = string.Empty;
@@ -22,9 +45,7 @@ namespace ExtensionManager.Shared.Actions
                     // Unless a valid folder path is specified for the argument of the
                     // initialDirectory parameter.
                     sfd.InitialDirectory =
-                        DetermineAppropriateInitialDirectory(
-                            initialDirectory
-                        );
+                        DetermineAppropriateInitialDirectory(initialDirectory);
 
                     sfd.DefaultExt = ".vsext";
                     sfd.FileName = "extensions";
