b53989dfa8f9fd7c08a273f6421696b465ef71e6
Brian Hart <brian.hart@xylogix.net>
2022-01-29 11:03:25 -0600

    Update Get.cs
    * Move from the ExtensionManager.Shared.Actions namespace to the ExtensionManager namespace, for consistency.
    * Added ImportFilePath static method

--- src/ExtensionManager.Shared/Actions/Get.cs
+++ src/ExtensionManager.Shared/Actions/Get.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Windows.Forms;
 
-namespace ExtensionManager.Shared.Actions
+namespace ExtensionManager
 {
     /// <summary>
     /// Exposes static methods to obtain values.
@@ -50,13 +50,71 @@ namespace ExtensionManager.Shared.Actions
                     sfd.DefaultExt = ".vsext";
                     sfd.FileName = "extensions";
                     sfd.Filter =
-                        "Visual Studio Extension List Export File (*.vsext)|*.vsext";
+                        "Visual Studio Extension List File (*.vsext)|*.vsext|All Files (*.*)|*.*";
                     sfd.CheckFileExists = sfd.CheckPathExists = true;
 
                     if (sfd.ShowDialog() != DialogResult.OK)
                         return result;
 
                     result = sfd.FileName;
+                }
+            }
+            catch
+            {
+                result = string.Empty;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Prompts the interactive user, with an Open dialog box, to determine from which
+        /// pathname the user would like the list of extensions to import read.
+        /// </summary>
+        /// <param name="initialDirectory">
+        /// (Optional.) The fully-qualified pathname of the
+        /// folder where the Save As dialog box should be focused when it initially opens.
+        /// <para />
+        /// If this value is blank, then the dialog box opens focused on the <c>This PC</c>
+        /// area of Windows instead.
+        /// </param>
+        /// <returns>
+        /// String containing the fully-qualified pathname that the user chose in
+        /// the dialog box; or, blank if an error occurred or the user clicked the
+        /// <strong>Cancel</strong> button.
+        /// </returns>
+        /// <remarks>
+        /// If the return value of this method is blank, then callers should not
+        /// proceed.
+        /// </remarks>
+        public static string ImportFilePath(string initialDirectory = "")
+        {
+            var result = string.Empty;
+
+            try
+            {
+                using (var ofd = new OpenFileDialog())
+                {
+                    ofd.Title = "Open Extension List for Import";
+                    ofd.RestoreDirectory =
+                        true; // do not reset the current working dir
+
+                    // Start the user out in "This PC" so they can then drill down from there,
+                    // Unless a valid folder path is specified for the argument of the
+                    // initialDirectory parameter.
+                    ofd.InitialDirectory =
+                        DetermineAppropriateInitialDirectory(initialDirectory);
+
+                    ofd.DefaultExt = ".vsext";
+                    ofd.FileName = "extensions";
+                    ofd.Filter =
+                        "Visual Studio Extension List File (*.vsext)|*.vsext|All Files (*.*)|*.*";
+                    ofd.CheckFileExists = ofd.CheckPathExists = true;
+
+                    if (ofd.ShowDialog() != DialogResult.OK)
+                        return result;
+
+                    result = ofd.FileName;
                 }
             }
             catch
