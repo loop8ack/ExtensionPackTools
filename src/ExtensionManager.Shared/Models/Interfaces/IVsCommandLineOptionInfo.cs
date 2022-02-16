5c5a08477e59b9539d5669c5511f450dd7df79a1
Brian Hart <brian.hart@xylogix.net>
2022-02-16 10:42:34 -0600

    Create IVsCommandLineOptionInfo.cs
    * Defines the properties of a POCO that encapsulates information about a VS command-line option.  The concrete POCO is more "POCO-esque," since we are also designing it with a dependency on the SVsAppCommandLine service, so it can "populate itself" upon construction.

---
+++ src/ExtensionManager.Shared/Models/Interfaces/IVsCommandLineOptionInfo.cs
@@ -0,0 +1,26 @@
+﻿namespace ExtensionManager
+{
+    /// <summary>
+    /// Defines the publicly-exposed methods and properties of an object that
+    /// encapsulates data about an option passed on the command line of the Visual
+    /// Studio Shell.
+    /// </summary>
+    public interface IVsCommandLineOptionInfo
+    {
+        /// <summary>
+        /// Gets or sets a string containing the name (case-insensitive) of the
+        /// command-line option to search for.
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// Gets a value that indicates whether the option was provided.
+        /// </summary>
+        bool IsProvided { get; }
+
+        /// <summary>
+        /// Gets the string value passed as an argument to the option, if any.
+        /// </summary>
+        string Value { get; }
+    }
+}
---
+++ src/ExtensionManager.Shared/Models/VsCommandLineOptionInfo.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace ExtensionManager
+{
+    /// <summary>
+    /// Encapsulates the information returned for a particular command-line option
+    /// passed to <c>devenv.exe</c>.
+    /// </summary>
+    public class VsCommandLineOptionInfo : IVsCommandLineOptionInfo
+    {
+        /// <summary>
+        /// Reference to an instance of an object that implements the
+        /// <see cref="T:Microsoft.VisualStudio.Shell.Interop.IVsAppCommandLine" />
+        /// interface.
+        /// </summary>
+        private readonly IVsAppCommandLine _vsAppCommandLine;
+
+        /// <summary>
+        /// Constructs a new instance of
+        /// <see cref="T:ExtensionManager.VsCommandLineOptionInfo" /> and returns a
+        /// reference to it.
+        /// </summary>
+        /// <param name="name">
+        /// (Required.)  String containing the case-insensitive name of
+        /// the command-line option to search for.
+        /// </param>
+        /// <param name="vsAppCommandLine">
+        /// (Required.) Reference to an instance of an object that implements the
+        /// <see cref="T:Microsoft.VisualStudio.Shell.Interop.IVsAppCommandLine" />
+        /// interface.
+        /// </param>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// Thrown if the required
+        /// parameter, <paramref name="vsAppCommandLine" />, is passed a
+        /// <see langword="null" /> value.
+        /// </exception>
+        public VsCommandLineOptionInfo(string name,
+            IVsAppCommandLine vsAppCommandLine)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException(
+                    "Value cannot be null or whitespace.", nameof(name)
+                );
+            Name = name;
+            _vsAppCommandLine = vsAppCommandLine ??
+                                throw new ArgumentNullException(
+                                    nameof(vsAppCommandLine)
+                                );
+
+            InitializeOption();
+        }
+
+        /// <summary>
+        /// Gets or sets a string containing the name (case-insensitive) of the
+        /// command-line option to search for.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets a value that indicates whether the option was provided.
+        /// </summary>
+        public bool IsProvided { get; private set; }
+
+        /// <summary>
+        /// Gets the string value passed as an argument to the option, if any.
+        /// </summary>
+        public string Value { get; private set; }
+
+        /// <summary>
+        /// Attempts to gather the info for a particular command-line option.
+        /// </summary>
+        private void InitializeOption()
+        {
+            try
+            {
+                ThreadHelper.ThrowIfNotOnUIThread();
+
+                if (string.IsNullOrWhiteSpace(Name)) return;
+                if (_vsAppCommandLine == null) return;
+
+                if (!ErrorHandler.Succeeded(
+                        _vsAppCommandLine.GetOption(
+                            Name, out var present, out var optionValue
+                        )
+                    )) return;
+                Value = optionValue;
+                IsProvided = present != 0;
+            }
+            catch (Exception ex)
+            {
+                // dump all the exception info to the log
+                Debug.WriteLine(ex);
+            }
+        }
+    }
+}
