02644c3c9373a39ce328d590c27b863facb3fc61
Brian Hart <brian.hart@xylogix.net>
2022-02-16 09:45:23 -0600

    Update IVsAppCommandLineService.cs
    * Reconsidered the method already defined.  It is not needed for a dependency-injected object.

--- src/ExtensionManager.Shared/Services/Interfaces/IVsAppCommandLineService.cs
+++ src/ExtensionManager.Shared/Services/Interfaces/IVsAppCommandLineService.cs
@@ -1,25 +1,9 @@
-﻿using Microsoft.VisualStudio.Shell.Interop;
-
-namespace ExtensionManager
+﻿namespace ExtensionManager
 {
     /// <summary>
     /// Defines the publicly-exposed methods and properties of a service that gets the
     /// command-line options passed to the Visual Studio Shell in which this extension
     /// is installed.
     /// </summary>
-    public interface IVsAppCommandLineService
-    {
-        /// <summary>
-        /// Gets a reference to an instance of an object that implements the
-        /// <see cref="T:Microsoft.VisualStudio.Shell.Interop.IVsAppCommandLine" />
-        /// interface from the global service provider for the Visual Studio Shell.
-        /// </summary>
-        /// <returns>
-        /// Reference to an instance of an object that implements the
-        /// <see cref="T:Microsoft.VisualStudio.Shell.Interop.IVsAppCommandLine" />
-        /// interface, or <see langword="null" /> if an error occurred while attempting to
-        /// obtain the reference.
-        /// </returns>
-        IVsAppCommandLine GetShellCommandLine();
-    }
+    public interface IVsAppCommandLineService { }
 }
